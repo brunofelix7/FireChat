6d62e11fccc1b0b75ce44e22209d9e9f00c1ae7a
Bruno Felix <bruno.felix@fuze.cc>
2023-05-05 00:27:50 -0300

    3. Setup Messages TableView

--- FireChat/Controller/ConversationsController.swift
+++ FireChat/Controller/ConversationsController.swift
@@ -1,9 +1,13 @@
 import Foundation
 import UIKit
 
+private let reuseIdentifier: String = "ConversationCell"
+
 class ConversationsController : UIViewController {
     
     // MARK: - Properties
+    
+    private let tableView = UITableView()
     
     // MARK: - Lifecycle
     
@@ -20,19 +24,20 @@ class ConversationsController : UIViewController {
     
     // MARK: - Helpers
     
-    func configureUI() {
+    private func configureUI() {
         view.backgroundColor = .white
         
-        configureNavigationBar()
+        configureNavBar()
+        configureTableView()
     }
     
-    func configureNavigationBar() {
+    private func configureNavBar() {
         let imageIcon = UIImage(systemName: "person.circle.fill")
         let appearance = UINavigationBarAppearance()
         
         appearance.configureWithOpaqueBackground()
         appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-        appearance.backgroundColor = .systemPurple
+        appearance.backgroundColor = .black
         
         navigationController?.navigationBar.standardAppearance = appearance
         navigationController?.navigationBar.compactAppearance = appearance
@@ -41,9 +46,48 @@ class ConversationsController : UIViewController {
         navigationController?.navigationBar.overrideUserInterfaceStyle = .dark
         navigationController?.navigationBar.isTranslucent = true
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+
         navigationItem.title = "Messages"
-        navigationItem.leftBarButtonItem = UIBarButtonItem(image: imageIcon, style: .plain,
-                                                           target: self, action: #selector(showProfile))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(
+            image: imageIcon,
+            style: .plain,
+            target: self,
+            action: #selector(showProfile))
+    }
+    
+    private func configureTableView() {
+        tableView.backgroundColor = .white
+        tableView.rowHeight = 80
+        tableView.tableFooterView = UIView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        tableView.frame = view.frame
+    }
+}
+
+extension ConversationsController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        cell.backgroundColor = .white
+        cell.textLabel?.text = "Test cell"
+        cell.textLabel?.textColor = .black
+        return cell
+    }
+    
+}
+
+extension ConversationsController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print(indexPath.row)
     }
 }
