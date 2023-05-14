13678890c8e842750a12683cca2196984d20f6ce
Bruno Felix <bruno.felix@fuze.cc>
2023-05-13 21:22:54 -0300

    6. Login Screen UI Continued

--- FireChat/Controller/Authentication/LoginController.swift
+++ FireChat/Controller/Authentication/LoginController.swift
@@ -10,26 +10,77 @@ class LoginController : UIViewController {
         return iv
     }()
     
-    private let emailContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .cyan
-        view.setHeight(height: 50)
-        return view
+    private lazy var emailContainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .clear
+        containerView.setHeight(height: 50)
+        
+        let icon = UIImageView()
+        icon.image = UIImage(systemName: "envelope")
+        icon.tintColor = .white
+        
+        containerView.addSubview(icon)
+        icon.centerY(inView: containerView)
+        icon.anchor(left: containerView.leftAnchor, paddingLeft: 8)
+        icon.setDimensions(height: 24, width: 28)
+        
+        containerView.addSubview(emailTextField)
+        emailTextField.anchor(top: icon.topAnchor,
+                              left: icon.rightAnchor,
+                              bottom: icon.bottomAnchor,
+                              right: containerView.rightAnchor,
+                              paddingLeft: 8,
+                              paddingRight: 8)
+        
+        return containerView
     }()
     
-    private let passwordContainerView: UIView = {
-        let view = UIView()
-        view.backgroundColor = .yellow
-        view.setHeight(height: 50)
-        return view
+    private lazy var passwordContainerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = .clear
+        containerView.setHeight(height: 50)
+        
+        let icon = UIImageView()
+        icon.image = UIImage(systemName: "lock")
+        icon.tintColor = .white
+        
+        containerView.addSubview(icon)
+        icon.centerY(inView: containerView)
+        icon.anchor(left: containerView.leftAnchor, paddingLeft: 8)
+        icon.setDimensions(height: 28, width: 28)
+        
+        containerView.addSubview(passwordTextField)
+        passwordTextField.anchor(top: icon.topAnchor,
+                              left: icon.rightAnchor,
+                              bottom: icon.bottomAnchor,
+                              right: containerView.rightAnchor,
+                              paddingLeft: 8,
+                              paddingRight: 8)
+        
+        return containerView
+    }()
+    
+    private let emailTextField: UITextField = {
+        let tfView = UITextField()
+        tfView.placeholder = "Email"
+        tfView.textColor = .white
+        return tfView
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let tfView = UITextField()
+        tfView.placeholder = "Password"
+        tfView.textColor = .white
+        tfView.isSecureTextEntry = true
+        return tfView
     }()
     
     private let loginButton: UIButton = {
         let button = UIButton()
-        button.setTitle("Login In", for: .normal)
-        button.layer.cornerRadius = 5
+        button.setTitle("Log In", for: .normal)
+        button.layer.cornerRadius = 6
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
-        button.backgroundColor = .systemRed
+        button.backgroundColor = .systemPink
         button.setHeight(height: 50)
         return button
     }()
@@ -42,13 +93,31 @@ class LoginController : UIViewController {
     private func configureUI() {
         configureNavBar()
         configureGradientLayer()
-        
+        configureLogoView()
+        configureStackView()
+    }
+    
+    private func configureNavBar() {
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+    }
+    
+    private func configureGradientLayer() {
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+    }
+    
+    private func configureLogoView() {
         view.addSubview(logoImage)
-        
         logoImage.centerX(inView: view)
         logoImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
         logoImage.setDimensions(height: 120, width: 120)
-        
+    }
+    
+    private func configureStackView() {
         let stack = UIStackView(arrangedSubviews: [
             emailContainerView,
             passwordContainerView,
@@ -66,18 +135,5 @@ class LoginController : UIViewController {
             paddingTop: 32,
             paddingLeft: 32,
             paddingRight: 32)
-    }
-    
-    private func configureNavBar() {
-        navigationController?.navigationBar.isHidden = true
-        navigationController?.navigationBar.barStyle = .black
-    }
-    
-    private func configureGradientLayer() {
-        let gradient = CAGradientLayer()
-        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemPink.cgColor]
-        gradient.locations = [0, 1]
-        view.layer.addSublayer(gradient)
-        gradient.frame = view.frame
     }
 }
